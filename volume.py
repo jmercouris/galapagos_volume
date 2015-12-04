07020fc6203b6d963d30fe3807a48e0e44b5a3c9
John Mercouris <jmercouris@gmail.com>
2015-12-04 23:10:16 +0100

    Custom buttons implemented

--- volume.py
+++ volume.py
@@ -3,10 +3,10 @@
 import os
 import subprocess
 import urwid
-
+# Represents an OSX audio device, e.g. input, output
 class AudioDevice:
     """
-    A class responsible for representing OSX audio devices
+    A class responsible for representing & manipulating OSX audio devices
     """
     def __init__(self, name, set_volume_command, get_volume_command):
         self.name = name
@@ -22,10 +22,10 @@ class AudioDevice:
         out, err = process.communicate('')
         return int(out)
 
+# Represents the Data in the program
 class VolumeModel:
     """
-    A class responsible for storing the data that will be displayed
-    on the graph, and keeping track of which mode is enabled.
+    Populating the default OSX AudioDevices
     """
     def __init__(self):
         # List of audio devices
@@ -43,66 +43,68 @@ class VolumeModel:
         set_volume_command = ['osascript', '-e', 'set volume input volume {}']
         device = AudioDevice("Input", set_volume_command, get_volume_command)
         audio_devices.append(device)
-        
     def get_audio_devices(self):
         return self.audio_devices
 
+# Custom Button
+class VolumeButton(urwid.Button):
+    def __init__(self, caption, callback):
+        super(VolumeButton, self).__init__("")
+        urwid.connect_signal(self, 'click', callback)
+        self._w = urwid.AttrMap(urwid.SelectableIcon(
+            [u'  \N{BULLET} ', caption], 2), None, 'selected')
+    def keypress(self, size, key):
+        if key == "q":
+            pass
+        else:
+            return urwid.Button.keypress(self, size, key)
+
+# Class VolumeView, handles drawing and input
 class VolumeView(urwid.WidgetWrap):
     """
     A class responsible for providing the application's interface and
     volume display.
     """
-    
     palette = [
-        ('body',         'black',      'light gray', 'standout'),
-        ('header',       'white',      'dark red',   'bold'),
-        ('screen edge',  'light blue', 'dark cyan'),
-        ('main shadow',  'dark gray',  'black'),
-        ('line',         'black',      'light gray', 'standout'),
-        ('bg background','light gray', 'black'),
-        ('bg 1',         'black',      'dark blue', 'standout'),
+        ('bg background','white', 'white'),
+        ('bg 1',         'black',      'black', 'standout'),
         ('bg 1 smooth',  'dark blue',  'black'),
-        ('bg 2',         'black',      'dark cyan', 'standout'),
-        ('bg 2 smooth',  'dark cyan',  'black'),
-        ('button normal','light gray', 'dark blue', 'standout'),
-        ('button select','white',      'dark green'),
-        ('line',         'black',      'light gray', 'standout'),
-        ('pg normal',    'white',      'black', 'standout'),
-        ('pg complete',  'white',      'dark magenta'),
-        ('pg smooth',     'dark magenta','black')
+        ('bg 2',         'black',      'dark gray', 'standout'),
+        ('bg 2 smooth',  'dark gray',  'black'),
+        ('button normal','black', 'white', 'light gray'),
+        ('button select','white',      'black'),
         ]
-
+    # Initialization
     def __init__(self, controller):
         self.controller = controller
         self.audio_devices = self.controller.get_audio_devices()
         urwid.WidgetWrap.__init__(self, self.main_window())
-        
+    # Bar Graph Configuration
     def bar_graph(self, smooth=False):
         satt = None
         if smooth:
             satt = {(1,0): 'bg 1 smooth', (2,0): 'bg 2 smooth'}
         w = urwid.BarGraph(['bg background','bg 1','bg 2'], satt=satt)
         return w
-        
     def button(self, t, fn):
-        w = urwid.Button(t, fn)
+        w = VolumeButton(t, fn)
         w = urwid.AttrWrap(w, 'button normal', 'button select')
         return w
-        
+    # Exit Program Onclick
     def exit_program(self, w):
         raise urwid.ExitMainLoop()
-        
+    # Update Graph View
     def update_graph(self, force_update=False):
         l = []
-        for n in range(10):
-            value = n
+        for index, device in enumerate(self.audio_devices):
+            volume = device.get_volume()
             # toggle between two bar types
-            if n & 1:
-                l.append([0,value])
+            if index & 1:
+                l.append([0,volume])
             else:
-                l.append([value,0])
-        self.graph.set_data(l,10)
-        
+                l.append([volume,0])
+        self.graph.set_data(l,100)
+    # Controls on the right hand side
     def graph_controls(self):
         l = [
             urwid.Divider(),
@@ -114,7 +116,7 @@ class VolumeView(urwid.WidgetWrap):
             ]
         w = urwid.ListBox(urwid.SimpleListWalker(l))
         return w
-        
+    # Configuration of the Main Window
     def main_window(self):
         self.graph = self.bar_graph()
         self.graph_wrap = urwid.WidgetWrap( self.graph )
@@ -126,6 +128,7 @@ class VolumeView(urwid.WidgetWrap):
         w = urwid.Padding(w,('fixed left',1),('fixed right',0))
         return w
 
+# Class VolumeController, serves as a view Controller
 class VolumeController:
     """
     A class responsible for setting up the model and view and running
@@ -135,7 +138,7 @@ class VolumeController:
         self.model = VolumeModel()
         self.view = VolumeView(self)
         self.view.update_graph(True)
-        
+    
     def get_audio_devices(self):
         return self.model.get_audio_devices()
 
@@ -143,13 +146,17 @@ class VolumeController:
         self.loop = urwid.MainLoop(self.view, self.view.palette)
         self.loop.run()
 
+# Initialize the VolumeController
 def main():
     VolumeController().main()
-
+# If Called from the command line
 if __name__ == "__main__":
     main()
 
-# Original Source: https://coderwall.com/p/22p0ja/set-get-osx-volume-mute-from-the-command-line
+################################################################################
+# Shell Commands to Change the Volume (This program executes these commands)
+################################################################################
+# Src: https://coderwall.com/p/22p0ja/set-get-osx-volume-mute-from-the-command-line
 # Get volume
 # # Echos a number from 0 to 100
 # osascript -e 'output volume of (get volume settings)'
