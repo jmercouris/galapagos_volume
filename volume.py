48f4b0454a50b4835247a5c73cb379945afb357f
John Mercouris <jmercouris@gmail.com>
2015-12-04 15:07:13 +0100

    Successfully setting volume

--- volume.py
+++ volume.py
@@ -32,14 +32,16 @@ def main():
     curses.start_color()
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-    # AudioDevice List Initialization
+    # AudioDevice Output
     get_volume_command = ['osascript', '-e', 'output volume of (get volume settings)']
-    set_volume_command = ['osascript', '-e', 'set volume output volume 50']
+    set_volume_command = ['osascript', '-e', 'set volume output volume {}']
     device = AudioDevice("Output", set_volume_command, get_volume_command)
     audio_devices.append(device)
-    audio_devices.append(device)
-    audio_devices.append(device)
-    audio_devices.append(device)
+
+    # AudioDevice Input
+    get_volume_command = ['osascript', '-e', 'input volume of (get volume settings)']
+    set_volume_command = ['osascript', '-e', 'set volume input volume {}']
+    device = AudioDevice("Input", set_volume_command, get_volume_command)
     audio_devices.append(device)
 
     # Main Input Loop
@@ -67,20 +69,28 @@ def draw_bars():
         draw_bar(default_padding, (bar_height * index) + default_padding, bar_height, device)
 
 def draw_bar(x, y, bar_height, device):
-    box = curses.newwin(bar_height, 80, y, x)
+    device.set_volume(29)
+    box = curses.newwin(bar_height, 50, y, x)
     box.bkgd(' ', curses.color_pair(1))
     box.immedok(True)
     box.box()
-    box.addstr("{} Volume:{}".format(device.name, device.volume))
+    box.addstr("{} Volume:{}".format(device.name, device.get_volume()))
 
 class AudioDevice:
     def __init__(self, name, set_volume_command, get_volume_command):
         self.name = name
         self.set_volume_command = set_volume_command
         self.get_volume_command = get_volume_command
-        self.volume = 0
-    def execute_command(self):
-        subprocess.Popen(self.system_command, stdout = subprocess.PIPE)
+
+    def set_volume(self, volume):
+        local_command = self.set_volume_command
+        local_command[2] = self.set_volume_command[2].format(volume)
+        process = subprocess.Popen(self.set_volume_command, stdout = subprocess.PIPE)
+        out, err = process.communicate('')
+    def get_volume(self):
+        process = subprocess.Popen(self.get_volume_command, stdout = subprocess.PIPE)
+        out, err = process.communicate('')
+        return out
 
 if __name__ == "__main__":
     main()
